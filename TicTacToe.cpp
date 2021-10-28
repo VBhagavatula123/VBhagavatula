099047c6f1dae03156085c3eb740cf736282dd24
VBhagavatula123 <90741649+VBhagavatula123@users.noreply.github.com>
2021-10-28 03:18:49 -0700

    Update TicTacToe.cpp

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -1,15 +1,18 @@
-
 #include <iostream>
 #include <cstring>
 
 using namespace std;
 
-//void chartMethod(char a[0],char a[1]);
 
-void printBoard(int arrParam[][3]);
-void printX(char arr[], int arrParam[][3]);
 
-struct board{
+void printBoard(int** arrParam);
+void printX(char* arr, int** arrParam);
+void printO(char* arr, int** arrParam);
+bool checkWinO(int** arrParam);
+bool checkWinO(int** arrParam);
+bool checkTie(int** arrParam);
+
+struct board{ // struct containing board and input strings
 
   int board1[3][3];
   char inputStringX[1];
@@ -19,24 +22,53 @@ struct board{
 
 int main()
 {
-  board turn1;
+
+  int xWincounter = 0;// win counters
+  int oWincounter = 0;
+
+  board game1;
 
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
-      turn1.board1[i][j] = 0;
+      game1.board1[i][j] = 0; //set everything in the board to 0
     }
   }
-  printBoard(turn1.board1);
-
-  cout << "please enter your X by choosing a number between 1 and 9" << endl;
-  cin >> turn1.inputStringX;
-
-  printX(turn1.inputStringX, turn1.board1);
+  printBoard(game1.board1);
 
 
 
+  while (checkWinX && checkWinO && checkTie != true) {
+    cout << "please enter your X by choosing a number between 1 and 9" << endl; // ask for inputs for X and O until checkWin = truw for x or O
+  cin >> game1.inputStringX;
+
+  printX(game1.inputStringX, game1.board1);
+
+  checkWinX(game1.board);
+  checkwinO(game1.board);
+  checkTie(game1.board);
+
+  cout << "please enter your O by choosing a number between 1 and 9" << endl;
+  cin >> game1.inputStringO;
+
+  printO(game1.inputStringO, game1.board1);
+
+   checkWinX(game1.board);
+  checkwinO(game1.board);
+  checkTie(game1.board);
 
 
+  }
+
+  if(checkWinX == true) {// if they equal true reset the board and add to the counter
+    xWincounter++;
+    cout << xWincounter << endl;
+    nextGame = new board();
+  }
+  if(checkWinO == true) {
+    oWincounter++;
+    cout << oWincounter << endl;
+    nextGame = new board();
+  }
 
 
 
@@ -44,7 +76,7 @@ int main()
   return 0;
 }
 
-void printBoard(int arrParam[][3]) {
+void printBoard(int** arrParam) {// function for printing board
   for (int i = 0; i < 3; i++) {
     for (int j = 0; j < 3; j++) {
       if(arrParam[i][j] == 0) {
@@ -62,7 +94,7 @@ void printBoard(int arrParam[][3]) {
   }
 }
 
-void printX(char arr[], int arrParam[][3]) {
+void printX(char* arr, int** arrParam) {//function for printing X
     if(arr[0] == 1 && arrParam[0][0] == 0) {
     arrParam[0][0] = 1;
   }
@@ -91,12 +123,127 @@ void printX(char arr[], int arrParam[][3]) {
     arrParam[2][2] = 1;
   }
 
-    printBoard(arrParam[][3]);
+    printBoard(int** arrParam);
 
 }
 
-void conversion(int arrParam[][3]) {
+void printO(char* arr, int** arrParam) {//function for printing O
+  if(arr[0] == 1 && arrParam[0][0] == 0) {
+    arrParam[0][0] = 2;
+  }
+    if(arr[0] == 1 && arrParam[0][1] == 0) {
+    arrParam[0][1] = 2;
+  }
+    if(arr[0] == 1 && arrParam[0][2] == 0) {
+    arrParam[0][2] = 2;
+  }
+    if(arr[0] == 1 && arrParam[1][0] == 0) {
+    arrParam[1][0] = 2;
+  }
+    if(arr[0] == 1 && arrParam[1][1] == 0) {
+    arrParam[1][1] = 2;
+  }
+    if(arr[0] == 1 && arrParam[1][2] == 0) {
+    arrParam[1][2] = 2;
+  }
+    if(arr[0] == 1 && arrParam[2][0] == 0) {
+    arrParam[2][0] = 2;
+  }
+    if(arr[0] == 1 && arrParam[2][1] == 0) {
+    arrParam[2][1] = 2;
+  }
+    if(arr[0] == 1 && arrParam[2][2] == 0) {
+    arrParam[2][2] = 2;
+  }
+
+    printBoard(int** arrParam);
 
 
 
 }
+
+bool checkWinX(int** arrParam) {// checks win for X
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (arrParam[0][0] == 1 && arrParam[0][1] == 1 && arrParam[0][2] == ) {
+        return true;
+      }
+       if (arrParam[1][0] == 1 && arrParam[1][1] == 1 && arrParam[1][2] == 1) {
+        return true;
+      }
+        if (arrParam[2][0] == 1 && arrParam[2][1] == 1 && arrParam[2][2] == 1) {
+        return true;
+      }
+        if (arrParam[0][0] == 1 && arrParam[1][0] == 1 && arrParam[2][0] == 1) {
+        return true;
+      }
+        if (arrParam[0][1] == 1 && arrParam[1][1] == 1 && arrParam[2][1] == 1) {
+        return true;
+      }
+        if (arrParam[0][2] == 1 && arrParam[1][2] == 1 && arrParam[2][2] == 1) {
+        return true;
+      }
+        if (arrParam[0][0] == 1 && arrParam[1][1] == 1 && arrParam[2][2] == 1) {
+        return true;
+      }
+        if (arrParam[0][2] == 1 && arrParam[1][1] == 1 && arrParam[2][0] == 1) {
+        return true;
+      }
+
+    }
+  }
+
+}
+
+bool checkWinO(int** arrParam) {//Checks win for O
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (arrParam[0][0] == 2 && arrParam[0][1] == 2 && arrParam[0][2] == 2) {
+        return true;
+      }
+       if (arrParam[1][0] == 2 && arrParam[1][1] == 2 && arrParam[1][2] == 2) {
+        return true;
+      }
+        if (arrParam[2][0] == 2 && arrParam[2][1] == 2 && arrParam[2][2] == 2) {
+        return true;
+      }
+        if (arrParam[0][0] == 2 && arrParam[1][0] == 2 && arrParam[2][0] == 2) {
+        return true;
+      }
+        if (arrParam[0][1] == 2 && arrParam[1][1] == 2 && arrParam[2][1] == 2) {
+        return true;
+      }
+        if (arrParam[0][2] == 2 && arrParam[1][2] == 2 && arrParam[2][2] == 2) {
+        return true;
+      }
+        if (arrParam[0][0] == 2 && arrParam[1][1] == 2 && arrParam[2][2] == 2) {
+        return true;
+      }
+        if (arrParam[0][2] == 2 && arrParam[1][1] == 2 && arrParam[2][0] == 2) {
+        return true;
+      }
+
+    }
+  }
+
+
+
+}
+
+bool checkTie(int** arrParam) {//Checks for tie
+
+  for (int i = 0; i < 3; i++) {
+    for (int j = 0; j < 3; j++) {
+      if (arrParam[0][0] != 0 && arrParam[0][1] != 0 && arrParam[0][2] != 0 && arrParam[0][0] != 0 && arrParam[0][0] != 0 && arrParam[0][0] != 0 && arrParam[0][0] != 0 && arrParam[0][0] != 0 && arrParam[0][0] != 0){
+        return true;
+      }
+    }
+  }
+
+
+}
+
+
+
