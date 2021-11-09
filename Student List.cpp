12691655790157497c29f1b65d8f604959d2f108
VBhagavatula123 <90741649+VBhagavatula123@users.noreply.github.com>
2021-11-08 23:32:56 -0800

    Update Student List.cpp

--- Student List.cpp
+++ Student List.cpp
@@ -1,59 +1,81 @@
+
 #include <iostream>
-#include <cstring>
 #include <vector>
+#include <cstring>
 
 using namespace std;
 
-void getStudentInfo(char a[20], char b[20]);
-
-struct student{
-
-  char display[8];
-  char first[20];
+struct StudentInfo {
+  char firstName[20];
   char lastName[20];
-  int studentID = 0;
-  float GPA = 0;
-
+  int id;
+  float GPA;
 };
 
+vector<StudentInfo> studentList;
 
-int main()
-{
 
-  vector<student*> v;
-  student s;
-  cout << "what is the first and last name of this student" << endl;
-  getStudentInfo(s.first, s.lastName);
+void ADD();
+void PRINT();
+void DELETE();
 
+int main() {
+      
+      
+  char inputString[7];
+  
+  do
+  {
+      cout << "What would you like to do?" << endl;
+      cin >> inputString;
+      if (strcmp(inputString, "ADD") == 0)
+        ADD();
+      else if (strcmp(inputString, "PRINT") == 0)
+        PRINT();
+      else if (strcmp(inputString, "DELETE") == 0)
+        DELETE();
+      else
+        break;
+   } while(strcmp(inputString, "QUIT") != 0);
+  
+   return 0;
 }
 
-void getStudentInfo(char a[20], char b[20]) {
-  cin >> a;
-  cin >> b;
-  cout << a << " " << b;
+void ADD() {
+    
+    StudentInfo newStudent;
+    cout << "Type student info:  firstName lastName studentId GPA" << endl;
+    cin >> newStudent.firstName;
+    cin >> newStudent.lastName;
+    cin >> newStudent.id;
+    cin >> newStudent.GPA;
+    
+    studentList.push_back(newStudent);
+    
 }
 
-
-void add (vector<student*> &in) {
-  student* s = new student();
-  in.push_back(s),
-    s-> first;
+void DELETE() {
+    int studentId;
+    cout << "what is your id to delte the student?" << endl;
+    cin >> studentId;
+    for(int i=0; i <studentList.size(); i++) {
+        if(studentList[i].id == studentId) {
+            studentList.erase(studentList.begin()+i-1);
+        }
+    }
 }
 
-void displayList(vector<student*> &in) {
-  cout << "display the list if you want to display the list, type display" << endl;
-  cin >> s.display;
-  if (s.display == "DISPLAY") {
-
-    cout << student << endl;
-
-  }
+void PRINT() {
+    
+    for(int i =0; i < studentList.size(); i++) {
+        cout << studentList[i].firstName << " ";
+        cout << studentList[i].lastName << " ";
+        cout << studentList[i].id << " ";
+        cout.setf(ios::fixed,ios::floatfield);
+        cout.setf(ios::showpoint);
+        cout.precision(2);
+        cout << studentList[i].GPA << endl; 
+    }
+    
 }
-
-
-
-
-
-
-
 
