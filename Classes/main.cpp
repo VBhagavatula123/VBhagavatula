6b364a20c94efb6731dc7f46b89d9f436c6c39b7
VBhagavatula123 <90741649+VBhagavatula123@users.noreply.github.com>
2021-12-08 07:50:11 -0800

    Update main.cpp

--- Classes/main.cpp
+++ Classes/main.cpp
@@ -1,4 +1,6 @@
-/* This is the main file, which includes all of the headerfiles, and the Vector of DigMedia(the parent class) pointers, containing our data.
+/* 
+Vivek, Classes, December 8th.
+This is the main file, which includes all of the headerfiles, and the Vector of DigMedia(the parent class) pointers, containing our data.
 the Info  gets passed in from the Parent and Sub-Classes to our vector using our commands ADD, LIST, DELETE, PRINT and SEARCH.
 much of this file has a similar framework and style to the previous project, StudentList*/
 
