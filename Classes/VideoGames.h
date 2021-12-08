666b4784065553a25665338080bfeb432df8609e
VBhagavatula123 <90741649+VBhagavatula123@users.noreply.github.com>
2021-12-08 00:23:48 -0800

    Update VideoGames.h

--- Classes/VideoGames.h
+++ Classes/VideoGames.h
@@ -1,3 +1,6 @@
+/* header file for 1 of 3 sub-classes. Must include the headerfile of the parent to inherit and follows similar structure. as the other 2.
+Protected objects are ones that are unique to the Media Type.*/
+
 #pragma once
 #include <iostream>
 #include <cstring>
