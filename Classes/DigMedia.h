5aaf423dd90806815922f57b9c78ea84dc348d38
VBhagavatula123 <90741649+VBhagavatula123@users.noreply.github.com>
2021-12-08 00:03:36 -0800

    Update DigMedia.h

--- Classes/DigMedia.h
+++ Classes/DigMedia.h
@@ -1,6 +1,6 @@
 /*the header file of the parent class. "a cookie cutter" for the .cpp file which contains the functionality of the class.
 Anything listed under public can be accessed outside of the class and anything Protected can only be accessed by the class and the sub-class.
-the year and the title is information that sub-classes are going to want so they are listed under protected, but not public because oublic also includes the main file.*/
+the year and the title is information that sub-classes are going to want so they are listed under protected, but not public because public also includes the main file.*/
 
 #pragma once
 #include <iostream>
