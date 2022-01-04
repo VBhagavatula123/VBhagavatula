242b740e5350977658379b57089169bfa8735e95
VBhagavatula123 <90741649+VBhagavatula123@users.noreply.github.com>
2022-01-03 23:44:31 -0800

    Update Room.h

--- Zuul/Room.h
+++ Zuul/Room.h
@@ -1,25 +1,40 @@
 #pragma once
 #include <iostream>
 #include <cstring>
+#include <vector>
+#include <map>
 using namespace std;
 
-
-struct item{
-    char name[];
-    char color[];
+struct Item {
+	char* name;
+	char* color;
 };
-
-
 
 class Room {
   public:
     Room();
     ~Room();
     
+    void setItems(vector<Item*> itemVect);
+    void setExits(map<char, int> ExMap);
     
+    void setNumber(int roomNumber);
+    void setName(char* name);
+    void setDesc(char* desc);
     
+    vector<Item*> getItems();
+    map<char, int> getExits();
+    
+    int getExitRoomNumber(char exitName);
+    
+    /*Item* pickupItem();
+    void dropItem(Item*);*/
+    
+  
   protected:
-    int roomNumber;
-    char roomDescription[];
-    vector<item*>itemptr;
+    int m_roomNumber;
+    char* m_roomName;
+    char* m_roomDescription;
+    vector<Item*> m_itemVector;
+    map<char, int> m_exMap;
 };
