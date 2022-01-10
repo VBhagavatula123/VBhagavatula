86ca258c4c8880224d08736c2e9d9374b73e9a6d
VBhagavatula123 <90741649+VBhagavatula123@users.noreply.github.com>
2022-01-10 14:41:10 -0800

    Update Room.h

--- Zuul/Room.h
+++ Zuul/Room.h
@@ -1,85 +1,47 @@
 /*
 
-This is the CPP file. This file contains all of the functions for the class, including the constructor, destructor,
-as well as functions for Items, Exits, Descriptions and Room name and number
+This is the header file for the room class. in the file, we declare the members of the class. The public members are needed for 
+main.cpp where as the protected ones are needed only for the functions in Room.cpp
 
 */
 
-
+#pragma once
 #include <iostream>
 #include <cstring>
-#include "Room.h"
+#include <vector>
 #include <map>
 using namespace std;
 
-Room::Room(int roomNum){
-    m_roomNumber = roomNum;
-}
+struct Item {
+	char* name;
+	char* color;
+};
 
-Room::~Room(){
+class Room {
+  public:
+    Room(int roomNum);
+    ~Room();
     
-}
-
-void Room::setItems(vector<Item*>ItemVect) {
-    m_itemVector = ItemVect;
-}
-
-void Room::setExits(map<char, int> ExMap) {
-    m_exMap = ExMap;
-}
-
-
-void Room::setName(char* name){
-    m_roomName = new char[10];
-    strcpy(m_roomName, name);
-}
-
-void Room::setDesc(char* desc){
-    m_roomDescription = new char[80];
-    strcpy(m_roomDescription, desc);
-}
-
-char* Room::getName(){
-    return m_roomName;
-}
-
-char* Room::getDesc(){
-    return m_roomDescription;
-}
+    void setItems(vector<Item*> itemVect);
+    void setExits(map<char, int> ExMap);
+    
+    void setName(char* name);
+    void setDesc(char* desc);
+    
+    char* getName();
+    char* getDesc();
+    vector<Item*> getItems();
+    map<char, int> getExits();
+    
+    int getExitRoomNumber(char exitName);
+    
+    void pickupItem(char* itemname, char* itemcolor, vector<Item*> &PlayerInventoryVectPtr);
+    void addItem(char* itemname, char* itemcolor);
   
-vector<Item*> Room::getItems(){
-    return m_itemVector;
-}
-    
-map<char, int> Room::getExits(){
-    return m_exMap;
-}
-    
-int Room::getExitRoomNumber(char exitLetter){
-    return m_exMap[exitLetter];
-}
-    
-
-void Room::addItem(char* itemname, char* itemcolor){
-    Item* newItem = new Item();
-    newItem->name = itemname;
-    newItem->color = itemcolor;
-    m_itemVector.push_back(newItem);
-}
-
-void Room::pickupItem(char* itemname, char* itemcolor, vector<Item*> &PlayerInventoryVectPtr) {
-    for(int i = 0; i<m_itemVector.size(); i++){
-        if(strcmp(m_itemVector[i]->name, itemname) == 0 && strcmp(m_itemVector[i]->color, itemcolor) == 0){
-            Item* newItem = new Item();
-            newItem->name = m_itemVector[i]->name;
-            newItem->color = m_itemVector[i]->color;
-            PlayerInventoryVectPtr.push_back(newItem);
-            //delete the data
-            delete m_itemVector[i];
-            //Remove the item from vector
-            m_itemVector.erase(m_itemVector.begin()+i);
-        }
-    }
-}
-
-
+  private:
+    int m_roomNumber;
+    char* m_roomName;
+    char* m_roomDescription;
+    vector<Item*> m_itemVector;
+    map<char, int> m_exMap;
+};
