38ad556811cb996cc358d9f326ed5f5e3b646035
VBhagavatula123 <90741649+VBhagavatula123@users.noreply.github.com>
2022-01-13 23:18:21 -0800

    Create Node.cpp

---
+++ LinkedList/Node.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include "Node.h"
+
+using namespace std;
+
+Node::Node(){
+    value = 0;
+    next = NULL;
+}
+
+Node::~Node(){
+    delete &value;
+    next = NULL;
+}
+
+void Node::setValue(int newvalue) {
+    value = newvalue;
+}
+
+int Node::getValue() {
+    return value;
+}
+
+void Node::setNext(Node* newnext){
+    next = newnext;
+}
+
+Node* Node::getNext(){
+    return next;
+}
+
+
