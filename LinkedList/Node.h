b09e05ead30d20032d9210ca50c7bdff3d5cbe5e
VBhagavatula123 <90741649+VBhagavatula123@users.noreply.github.com>
2022-01-16 22:38:00 -0800

    Update Node.h

--- LinkedList/Node.h
+++ LinkedList/Node.h
@@ -1,28 +1,29 @@
+//all of these functions are the functions that are needed for the main function, therefore, every function will be declared public
+// private members are members that we are using for our functions over in Node.cpp.
+//for the Node constructor we need to pass a pointer to the new instantiation of a student class, which just copies the input strings from main and stores them
+// for the next functions setNext sets(as in establishes) the next node, where as getNext gets the next node it as in(grabs for use) by just reurning next which is of type Node*
+//pragma once is a line of code to clean up complilation errors.
+//getStudent is to return the specific student info of type Student*, which was instantiatied in Node()
+
+#pragma once
 #include <iostream>
 #include <cstring>
+#include "Student.h"
 
 using namespace std;
+
 
 class Node {
     public:
     Node();
     ~Node(); 
-    void setValue(int newvalue);
-    int getValue();
-    void setNext(Node* newnext);
+    void setNext(Node*);
+    Node(Student*);
     Node* getNext();
+    Student* getStudent();
     
     
     private:
-    int value;
     Node* next;
+    Student* stdinfo;
 };
-
-//Node* getNext()
-//Student* getStudent()
-
-//Node(Student*) 
-
-//~Node() 
-
-//void setNext(Node*)
