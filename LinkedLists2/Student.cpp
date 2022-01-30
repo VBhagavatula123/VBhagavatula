def4886769eeb8c262439fcec33fd994aa994f32
VBhagavatula123 <90741649+VBhagavatula123@users.noreply.github.com>
2022-01-30 00:14:31 -0800

    Create Student.cpp

---
+++ LinkedLists2/Student.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Student.h"
+
+using namespace std;
+
+Student::Student(char* first, char* last, int ID, float GPA){
+  strcpy(s_firstName, first);
+  strcpy(s_lastName, last);
+  s_id = ID;
+  s_GPA = GPA;
+}
+
+Student::~Student() {
+    
+}
