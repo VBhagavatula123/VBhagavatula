5962e5fe0c20f721c9eaaf457c9d92fb7c9ee8e2
VBhagavatula123 <90741649+VBhagavatula123@users.noreply.github.com>
2022-01-30 00:14:09 -0800

    Create Student.h

---
+++ LinkedLists2/Student.h
@@ -0,0 +1,19 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+
+
+using namespace std;
+
+class Student {
+    public:
+    
+    char s_firstName[10];
+    char s_lastName[10];
+    int s_id;
+    float s_GPA;
+    void getStudentInfo();
+    Student(char* first,char* last, int ID, float GPA);
+    ~Student(); 
+};
+
