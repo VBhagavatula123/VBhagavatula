f31027b0ed15b7b45440b0ae6e92f9b641dc7dd3
VBhagavatula123 <90741649+VBhagavatula123@users.noreply.github.com>
2022-01-31 14:20:58 -0800

    Update Student.h

--- LinkedLists2/Student.h
+++ LinkedLists2/Student.h
@@ -5,15 +5,15 @@
 
 using namespace std;
 
-class Student {
-    public:
-    
-    char s_firstName[10];
-    char s_lastName[10];
-    int s_id;
-    float s_GPA;
-    void getStudentInfo();
-    Student(char* first,char* last, int ID, float GPA);
-    ~Student(); 
-};
+class Student
+{
+public:
 
+  char s_firstName[10];
+  char s_lastName[10];
+  int s_id;
+  float s_GPA;
+  void getStudentInfo ();
+    Student (char *first, char *last, int ID, float GPA);
+   ~Student ();
+};
