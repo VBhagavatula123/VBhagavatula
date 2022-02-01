262029bd88d46ce9870061661eff038d74bd28a5
VBhagavatula123 <90741649+VBhagavatula123@users.noreply.github.com>
2022-02-01 01:06:58 -0800

    Update Node.h

--- LinkedLists2/Node.h
+++ LinkedLists2/Node.h
@@ -1,25 +1,27 @@
-#pragma once
+//header file for node
+#ifndef NODE_H
+#define NODE_H
 #include <iostream>
-#include <cstring>
 #include "Student.h"
 
 using namespace std;
 
-
 class Node
 {
-public:
-  Node ();
-  ~Node ();
-  void setNext (Node *);
-    Node (Student *);
-  Node *getNext ();
-  Student *getStudent ();
-  /*int getID();*/
-  Node *next;
+ public:
+  //define functions
+  Node(); // default constructor
+  Node(Student* newStudent); // constructor, set student pointer to new student
+  Node* getNext(); //get next Node pointer
+  Student* getStudent(); // get Student pointer
+  void setNext(Node* newNode); // set next pointer to corresponding node pointer
+  ~Node(); // destructor
 
-
-private:
-    Student * stdinfo;
-
+ private:
+  //variables
+  Node* next;
+  Student* student;
+  
 };
+
+#endif
