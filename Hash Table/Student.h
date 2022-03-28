d843b34d6669765fb13ef70a19c9bc4e4ca2efa1
VBhagavatula123 <90741649+VBhagavatula123@users.noreply.github.com>
2022-03-27 19:40:09 -0700

    Create Student.h

---
+++ Hash Table/Student.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <iostream>
+#include <cstring>
+
+//This file has allthe members for a student, all are made public to use in the node class.
+
+using namespace std;
+
+class Student
+{
+public:
+  Student* next; 
+  char s_firstName[30];
+  char s_lastName[30];
+  int s_id;
+  float s_GPA;
+  void getStudentInfo ();
+    Student (char *first, char *last, int ID, float GPA);
+   ~Student ();
+   Student** getTable();
+};
