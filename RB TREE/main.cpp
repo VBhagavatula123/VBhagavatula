5d9e538157809cb0e9d5762309e7f6cbe6be2bf7
VBhagavatula123 <90741649+VBhagavatula123@users.noreply.github.com>
2022-04-11 11:48:29 -0700

    Create main.cpp

---
+++ RB TREE/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+struct node {
+    int val;
+    char color[5];
+    node* right;
+    node* left;
+}
+
+void insert() {
+    
+}
+
+int main()
+{
+    node* tree = NULL;
+    
+    while(true) {
+        if(strcmp(inputString, 'A') == 0) {
+            
+        }
+        else if(strcmp(inputString, 'D') == 0) {
+            
+        }
+        else if(strcmp(inputString, 'S') == 0) {
+            
+        }
+        else if(strcmp(inputString, 'Q') == 0) {
+            exit(0);
+        }
+    }
+
+    return 0;
+}
